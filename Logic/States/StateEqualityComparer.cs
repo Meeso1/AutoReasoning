53b16c36d055308bbd07abc3242cef252e2bf0e9
Karas <maciej2.karaskiewicz@gmail.com>
2025-05-30 13:03:14 +0200

    state generations with constraints consideration

---
+++ Logic/States/StateEqualityComparer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.States.Models;
+
+namespace Logic.States;
+
+public class StateEqualityComparer : IEqualityComparer<State>
+{
+    public bool Equals(State? x, State? y)
+    {
+        if (x is null && y is null) return true;
+        if (x is null || y is null) return false;
+
+        if (x.FluentValues.Count != y.FluentValues.Count) return false;
+
+        return x.FluentValues.All(kvp =>
+            y.FluentValues.TryGetValue(kvp.Key, out var value) &&
+            kvp.Value == value);
+    }
+
+    public int GetHashCode(State obj)
+    {
+        var hash = new HashCode();
+        foreach (var kvp in obj.FluentValues.OrderBy(x => x.Key.GetHashCode()))
+        {
+            hash.Add(kvp.Key);
+            hash.Add(kvp.Value);
+        }
+        return hash.ToHashCode();
+    }
+}
